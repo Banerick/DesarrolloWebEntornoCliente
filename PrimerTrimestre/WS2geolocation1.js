d01ef2b3a848047f0151dd48d04cd52a0265102d
Banerick <jegarle44@gmail.com>
2017-10-09 14:06:12 +0200

    Ejercicios 1 al 4

---
+++ PrimerTrimestre/WS2geolocation1.js
@@ -0,0 +1,36 @@
+//Ejercicios del 1 al 3
+/*
+function miLocalizacion() {
+var curPos;
+navigator.geolocation.getCurrentPosition(function(position) {
+curPos = position.coords;
+console.log("Posicion = lat: " + (curPos.latitude+i)
++ " || long: " + (curPos.longitude+i));
+});
+}
+*/
+
+function obtDistance(lat1, lon1, lat2, lon2) {
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 +
+  c(lat1 * p) * c(lat2 * p) *
+  (1 - c((lon2 - lon1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+var lastPos;
+var distance = 0;
+function initMap() {
+  navigator.geolocation.getCurrentPosition(function (position){
+    lastPos = position.coords
+
+    navigator.geolocation.watchPosition(function(position) {
+      distance += obtDistance(lastPos.latitude,lastPos.longitude,
+        position.coords.latitude,position.coords.longitude);
+        lastPos = position.coords;
+        console.log(distance);
+      });
+    });
+  };
+initMap();
